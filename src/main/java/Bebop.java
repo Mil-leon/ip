6c60920390672d91503d6958af2305ddefb9a424
Mil-leon <noellim30@gmail.com>
2025-01-22 17:58:00 +0800

    Add Tasklist

--- src/main/java/Bebop.java
+++ src/main/java/Bebop.java
@@ -1,19 +1,29 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Bebop {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        ArrayList<String> taskList = new ArrayList(100);
         String logo = "\tWhat can I do for you today? \n" +
                 "\t__________________________________";
         System.out.println("\tHowdy! How's it going? \n" + logo);
         while (true) {
-            String input = s.nextLine();
+            String input = scan.nextLine();
             if (Objects.equals(input, "bye")) {
                 System.out.println("\tHave a nice day :D, see you soon!");
                 break;
+            } else if (Objects.equals(input, "list")) {
+                int i = 0;
+                for (String task : taskList) {
+                    System.out.println("\t" + (i + 1) + ": "  + taskList.get(i));
+                    i++;
+                }
+                System.out.println("\t__________________________________");
             } else {
-                System.out.println("\t" + input +
+                taskList.add(input);
+                System.out.println("\t" + "added: " + input +
                         "\n\t__________________________________");
             }
         }
