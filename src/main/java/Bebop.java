23dc69336505508ae0c105d105c4fd7cbdf33f45
Mil-leon <noellim30@gmail.com>
2025-01-22 17:47:02 +0800

    Add echo feature to the chatbot

--- src/main/java/Bebop.java
+++ src/main/java/Bebop.java
@@ -1,7 +1,21 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Bebop {
     public static void main(String[] args) {
-        String logo = "What can I do for you today? \n" +
-                "See you next buddy!";
-        System.out.println("Howdy! How's it going? \n" + logo);
+        Scanner s = new Scanner(System.in);
+        String logo = "\tWhat can I do for you today? \n" +
+                "\t__________________________________";
+        System.out.println("\tHowdy! How's it going? \n" + logo);
+        while (true) {
+            String input = s.nextLine();
+            if (Objects.equals(input, "bye")) {
+                System.out.println("\tHave a nice day :D, see you soon!");
+                break;
+            } else {
+                System.out.println("\t" + input +
+                        "\n\t__________________________________");
+            }
+        }
     }
 }
